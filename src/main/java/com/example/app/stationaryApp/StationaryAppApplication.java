24cc15a01073efdb5d76c5a1aebbd8a5e2bcc01e
Krishan Kumar Pareek <krishan.pareek@tavant.com>
2021-12-01 12:12:32 +0530

    Initial Commit

---
+++ src/main/java/com/example/app/stationaryApp/StationaryAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.app.stationaryApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationaryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationaryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/app/stationaryApp/StationaryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app.stationaryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationaryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
