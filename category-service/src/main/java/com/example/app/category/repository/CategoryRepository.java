4bb492440cd377c5d3b11d78497fc61aaec74c38
Akshar-2000 <aksharreddy26@gmail.com>
2021-12-06 09:56:53 +0530

    changes

--- category-service/src/main/java/com/example/app/category/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.app.category.repository;
-
-import com.example.app.category.entities.Category;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends CrudRepository<Category, Integer> {
-}
--- customer-service/src/main/java/com/example/app/customer/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.app.customer.repository;
-
-import com.example.app.customer.entities.Customer;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends CrudRepository<Customer, Integer> {
-}
