046423ce9904d669a1369f7e21e5f44d4a4c3fc5
Venkata Katragadda <venkata.katragadda@tavant.com>
2021-12-08 10:53:24 +0530

    base components created

--- stationary-shop/src/app/app.module.ts
+++ stationary-shop/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NavComponent } from './components/nav/nav.component';
+import { LoginComponent } from './components/login/login.component';
+import { PaymentComponent } from './components/payment/payment.component';
+import { AccountComponent } from './components/account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductsComponent,
+    NavComponent,
+    LoginComponent,
+    PaymentComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
