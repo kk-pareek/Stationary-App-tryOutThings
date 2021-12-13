0062d7f906e51918a4999a8e4b29e1644f06f35c
Krishan Kumar Pareek <krishan.pareek@tavant.com>
2021-12-13 15:02:54 +0530

    Added RestConfig for Customer-service

--- customer-service/src/main/java/com/example/app/customer/RestConfig.java
+++ customer-service/src/main/java/com/example/app/customer/RestConfig.java
@@ -1,2 +1,13 @@
-package com.example.app.customer;public class RestConfig {
+package com.example.app.customer;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Component
+public class RestConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+    }
 }
